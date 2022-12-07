7ada732f8213e8bb435a565d13e9966c636d6434
Alexey Matveev <tippet@yandex.ru>
2022-12-07 09:08:40 +0300

    black 120

--- retry/api.py
+++ retry/api.py
@@ -10,9 +10,9 @@ from decorator import decorator
 logging_logger = logging.getLogger(__name__)
 
 
-def __retry_internal(f, exceptions=Exception, tries=-1, delay=0,
-                     max_delay=None, backoff=1, jitter=0,
-                     logger=logging_logger):
+def __retry_internal(
+    f, exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, jitter=0, logger=logging_logger
+):
     """
     Executes a function and retries it if it failed.
 
@@ -38,7 +38,7 @@ def __retry_internal(f, exceptions=Exception, tries=-1, delay=0,
                 raise
 
             if logger is not None:
-                logger.warning('%s, retrying in %s seconds...', e, _delay)
+                logger.warning("%s, retrying in %s seconds...", e, _delay)
 
             time.sleep(_delay)
             _delay *= backoff
@@ -52,9 +52,9 @@ def __retry_internal(f, exceptions=Exception, tries=-1, delay=0,
                 _delay = min(_delay, max_delay)
 
 
-async def __retry_internal_async(f, exceptions=Exception, tries=-1, delay=0,
-                                 max_delay=None, backoff=1, jitter=0,
-                                 logger=logging_logger):
+async def __retry_internal_async(
+    f, exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, jitter=0, logger=logging_logger
+):
     _tries, _delay = tries, delay
     while _tries:
         try:
@@ -65,7 +65,7 @@ async def __retry_internal_async(f, exceptions=Exception, tries=-1, delay=0,
                 raise
 
             if logger is not None:
-                logger.warning('%s, retrying in %s seconds...', e, _delay)
+                logger.warning("%s, retrying in %s seconds...", e, _delay)
 
             await asyncio.sleep(_delay)
             _delay *= backoff
@@ -79,8 +79,7 @@ async def __retry_internal_async(f, exceptions=Exception, tries=-1, delay=0,
                 _delay = min(_delay, max_delay)
 
 
-def retry(exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1,
-          jitter=0, logger=logging_logger):
+def retry(exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, jitter=0, logger=logging_logger):
     """Returns a retry decorator.
 
     :param exceptions: an exception or a tuple of exceptions to catch. default: Exception.
@@ -95,25 +94,35 @@ def retry(exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1,
     :param async: If True then return generator
     :returns: a retry decorator.
     """
+
     @decorator
     def retry_decorator(f, *fargs, **fkwargs):
         args = fargs if fargs else list()
         kwargs = fkwargs if fkwargs else dict()
         if not asyncio.iscoroutinefunction(f):
-            return __retry_internal(partial(f, *args, **kwargs), exceptions,
-                                    tries, delay, max_delay, backoff, jitter,
-                                    logger)
+            return __retry_internal(
+                partial(f, *args, **kwargs), exceptions, tries, delay, max_delay, backoff, jitter, logger
+            )
         else:
-            return __retry_internal_async(partial(f, *args, **kwargs),
-                                          exceptions, tries, delay, max_delay,
-                                          backoff, jitter, logger)
+            return __retry_internal_async(
+                partial(f, *args, **kwargs), exceptions, tries, delay, max_delay, backoff, jitter, logger
+            )
 
     return retry_decorator
 
 
-def retry_call(f, fargs=None, fkwargs=None, exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1,
-               jitter=0,
-               logger=logging_logger):
+def retry_call(
+    f,
+    fargs=None,
+    fkwargs=None,
+    exceptions=Exception,
+    tries=-1,
+    delay=0,
+    max_delay=None,
+    backoff=1,
+    jitter=0,
+    logger=logging_logger,
+):
     """
     Calls a function and re-executes it if it failed.
 
